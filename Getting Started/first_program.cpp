92987e143a2d57aef8604ae89941a608157c10be
Subrata Banerjee <subrata.altavolant@gmail.com>
2022-07-31 20:18:54 +0530

    first_program

---
+++ Getting Started/first_program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// 1. All C++ program must have a main function, this is the starting point for C++ program execution
+// 2. All C++ program must have only 1 main() function
+int main()
+{
+    // all statement in C++ must have an ending semi-colon
+    int favourite_number;
+
+    std::cout << "Enter your favourite number (1-100): ";
+    std::cin >> favourite_number;
+    std::cout << "Amazing that's my favourite number too!" << std::endl;
+
+    // returning 0 because the return type for the main function is integer
+    return 0;
+}
